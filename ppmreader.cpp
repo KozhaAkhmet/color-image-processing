87287b6f2658cd7447a68e25908a6672e1f2d47a
KozhaAkhmet <kozha24082001@gmail.com>
2023-05-27 10:26:45 +0000

    Working Opencv

--- ppmreader.cpp
+++ ppmreader.cpp
@@ -1,13 +1,14 @@
-#include<stdio.h>
+#include <stdio.h>
 // #include<conio.h>
-#include<stdlib.h>
-#include<math.h>
-#include<ctype.h>
-#include<string.h>
+#include <stdlib.h>
+#include <math.h>
+#include <ctype.h>
+#include <string.h>
 #include <fcntl.h>
 #include <malloc.h>
 #include <math.h>
-#include <opencv2/core>
+#include <opencv2/opencv.hpp>
+#include <opencv2/core.hpp>
 
 #define PI 3.1415926535897932384626433832795
 #pragma pack(1)
@@ -24,112 +25,113 @@ struct ppm_header
 struct ppm_file
 {
 	struct ppm_header *pheader;
-	unsigned char *rdata,*gdata,*bdata;
+	unsigned char *rdata, *gdata, *bdata;
 };
 
-void write_image(char *filename,struct ppm_file *image)
+void write_image(char *filename, struct ppm_file *image)
 {
 	FILE *fp;
-	int i,max=0;
-	fp=fopen(filename,"wb");
-	fputc(image->pheader->pgmtype1,fp);
-	fputc(image->pheader->pgmtype2,fp);
-	fputc('\n',fp);
-	fprintf(fp,"%d %d\n",image->pheader->pwidth,image->pheader->pheight);
-	fprintf(fp,"%d\n",255/*max*/);
-	for(i=0;i<image->pheader->pwidth*image->pheader->pheight;i++)
+	int i, max = 0;
+	fp = fopen(filename, "wb");
+	fputc(image->pheader->pgmtype1, fp);
+	fputc(image->pheader->pgmtype2, fp);
+	fputc('\n', fp);
+	fprintf(fp, "%d %d\n", image->pheader->pwidth, image->pheader->pheight);
+	fprintf(fp, "%d\n", 255 /*max*/);
+	for (i = 0; i < image->pheader->pwidth * image->pheader->pheight; i++)
 	{
-		fwrite(&image->rdata[i],1,1,fp);
-		fwrite(&image->gdata[i],1,1,fp);
-		fwrite(&image->bdata[i],1,1,fp);
+		fwrite(&image->rdata[i], 1, 1, fp);
+		fwrite(&image->gdata[i], 1, 1, fp);
+		fwrite(&image->bdata[i], 1, 1, fp);
 	}
 	fclose(fp);
 }
 
-void get_image_data(char *filename, struct ppm_file *image ) 
+void get_image_data(char *filename, struct ppm_file *image)
 {
-	FILE* fp;
-	int i=0;
+	FILE *fp;
+	int i = 0;
 	char temp[256];
-	image->pheader=(struct ppm_header *)malloc(sizeof(struct ppm_header));
-	fp = fopen(filename, "rb" );
-	if (fp==NULL) 
+	image->pheader = (struct ppm_header *)malloc(sizeof(struct ppm_header));
+	fp = fopen(filename, "rb");
+	if (fp == NULL)
 	{
 		printf("File is not opened: %s.\n\n", filename);
 		exit(1);
 	}
-	printf ("The PPM File : %s...\n", filename);
-	fscanf (fp, "%s", temp);
-	if (strcmp(temp, "P6") == 0) 
+	printf("The PPM File : %s...\n", filename);
+	fscanf(fp, "%s", temp);
+	if (strcmp(temp, "P6") == 0)
 	{
-		image->pheader->pgmtype1=temp[0];
-		image->pheader->pgmtype2=temp[1];
-		fscanf (fp, "%s", temp);
-		if (temp[0]=='#') 
+		image->pheader->pgmtype1 = temp[0];
+		image->pheader->pgmtype2 = temp[1];
+		fscanf(fp, "%s", temp);
+		if (temp[0] == '#')
 		{
-			while(fgetc(fp)!='\n');
-			fscanf (fp, "%d %d\n",&image->pheader->pwidth,&image->pheader->pheight);
-			fscanf (fp, "%d\n", &image->pheader->pmax);
+			while (fgetc(fp) != '\n')
+				;
+			fscanf(fp, "%d %d\n", &image->pheader->pwidth, &image->pheader->pheight);
+			fscanf(fp, "%d\n", &image->pheader->pmax);
 		}
 		else
 		{
-			sscanf (temp, "%d", &image->pheader->pwidth);
-			fscanf (fp, "%d", &image->pheader->pheight);
-			fscanf (fp, "%d", &image->pheader->pmax);
+			sscanf(temp, "%d", &image->pheader->pwidth);
+			fscanf(fp, "%d", &image->pheader->pheight);
+			fscanf(fp, "%d", &image->pheader->pmax);
 		}
-		image->rdata=(unsigned char *)malloc(image->pheader->pheight*image->pheader->pwidth*sizeof(unsigned char));
-		image->gdata=(unsigned char *)malloc(image->pheader->pheight*image->pheader->pwidth*sizeof(unsigned char));
-		image->bdata=(unsigned char *)malloc(image->pheader->pheight*image->pheader->pwidth*sizeof(unsigned char));
-		if (image->rdata==NULL) printf("Memory problem\n");
-		for(i=0;i<image->pheader->pwidth*image->pheader->pheight;i++)
+		image->rdata = (unsigned char *)malloc(image->pheader->pheight * image->pheader->pwidth * sizeof(unsigned char));
+		image->gdata = (unsigned char *)malloc(image->pheader->pheight * image->pheader->pwidth * sizeof(unsigned char));
+		image->bdata = (unsigned char *)malloc(image->pheader->pheight * image->pheader->pwidth * sizeof(unsigned char));
+		if (image->rdata == NULL)
+			printf("Memory problem\n");
+		for (i = 0; i < image->pheader->pwidth * image->pheader->pheight; i++)
 		{
-			fread(&image->rdata[i],1,1,fp);
-			fread(&image->gdata[i],1,1,fp);
-			fread(&image->bdata[i],1,1,fp);
+			fread(&image->rdata[i], 1, 1, fp);
+			fread(&image->gdata[i], 1, 1, fp);
+			fread(&image->bdata[i], 1, 1, fp);
 		}
 	}
-	else 
+	else
 	{
-		printf ("\nError! The file is not a PPM file");
+		printf("\nError! The file is not a PPM file");
 		exit(1);
 	}
 	fclose(fp);
 }
 
-Mat equalizeIntensity(const Mat& inputImage)
+cv::Mat equalizeIntensity(const cv::Mat inputImage)
 {
-    if(inputImage.channels() >= 3)
-    {
-        Mat ycrcb;
-        cvtColor(inputImage,ycrcb,CV_BGR2YCrCb);
+	if (inputImage.channels() >= 3)
+	{
+		cv::Mat ycrcb;
+		cvtColor(inputImage, ycrcb, CV_BGR2YCrCb);
 
-        vector<Mat> channels;
-        split(ycrcb,channels);
+		std::vector<cv::Mat> channels;
+		split(ycrcb, channels);
 
-        equalizeHist(channels[0], channels[0]);
+		equalizeHist(channels[0], channels[0]);
 
-        Mat result;
-        merge(channels,ycrcb);
-        cvtColor(ycrcb,result,CV_YCrCb2BGR);
+		cv::Mat result;
+		merge(channels, ycrcb);
+		cvtColor(ycrcb, result, CV_YCrCb2BGR);
 
-        return result;
-    }
+		return result;
+	}
 
-    return Mat();
+	return cv::Mat();
 }
 
-
 int main()
-{	
+{
 	struct ppm_file picture;
 	int i;
-	get_image_data("mandrill.ppm",&picture);
-	//Information of image
-	printf("pgmtype...=%c%c\n",picture.pheader->pgmtype1,picture.pheader->pgmtype2);
-	printf("width...=%d\n",picture.pheader->pwidth);
-	printf("height...=%d\n",picture.pheader->pheight);
-	printf("max gray level...=%d\n",picture.pheader->pmax);
+	get_image_data("mandrill.ppm", &picture);
+	// Information of image
+	printf("pgmtype...=%c%c\n", picture.pheader->pgmtype1, picture.pheader->pgmtype2);
+	printf("width...=%d\n", picture.pheader->pwidth);
+	printf("height...=%d\n", picture.pheader->pheight);
+	printf("max gray level...=%d\n", picture.pheader->pmax);
 	equalizeIntensity(picture);
-	write_image("pnr.ppm",&picture);
+	write_image("pnr.ppm", &picture);
 	return 0;
-} 
+}
